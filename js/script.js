d38ed8f940d4dcbadb3c8d4d0129df4d3d200129
unknown <csgo.roby@gmail.com>
2018-12-14 13:43:58 +0300

    D

--- js/script.js
+++ js/script.js
@@ -9,3 +9,43 @@
       }
     });
   });
+
+  $(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+      loop: false,
+      margin: 10,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true
+        },
+        600: {
+          items: 3,
+          nav: false
+        },
+        1000: {
+          items: 0,
+          nav: false,
+          loop: false
+        }
+      }
+    })
+  });
+
+
+  $(document).ready(function () {
+    $("#mmenu").mmenu({
+      "slidingSubmenus": false,
+      navbar: {
+        title: false
+      }
+    });
+
+    var api = $("#mmenu").data( "mmenu" );
+    api.bind('open:finish', function () {
+      $('.hamburger').addClass('is-active');
+    }).bind('close:finish', function(){
+        $('.hamburger').removeClass('is-active');
+      });
+  });
